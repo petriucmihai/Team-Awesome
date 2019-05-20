eed635fed19a6cb483a8cfd454deb9e340e8cbae
petriucmihai <mihaitp10@gmail.com>
2019-05-20 10:58:28 -0700

    Added fields to the ResourcesChartModel for the charts on the resources page

--- JaunDetect/JaunDetect/Models/ResourcesChartModel.cs
+++ JaunDetect/JaunDetect/Models/ResourcesChartModel.cs
@@ -7,5 +7,36 @@ namespace JaunDetect.Models
 {
     public class ResourcesChartModel
     {
+        // Months that will be shown on a chart over time
+        public string[] Months { get; set; }
+
+        // Number of test strips used
+        public int[] TestStripUsages { get; set; }
+
+        // List of the clinics at which the data was tracked
+        public string[] Clinics { get; set; }
+
+        // Number of patients sent from a clinic to a nearby hospital
+        public int[] HospitalPatients { get; set; }
+
+        // Price of a single test strip
+        public double TestStripPrice { get; set; }
+
+        // Cost of the total amount of test strips used at a clinic
+        public double[] TestStripCosts { get; set; }
+
+        public ResourcesChartModel()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Intialize
+        /// Sets default values, such as DateTime as needed by the system
+        /// </summary>
+        public void Initialize()
+        {
+            // None right now...
+        }
     }
 }
