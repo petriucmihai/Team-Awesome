082f747efd5a5e0cde46d4ef007750f00b548691
petriucmihai <mihaitp10@gmail.com>
2019-05-20 11:02:46 -0700

    Methods for Resources page charts added to IDataRepository interface

--- JaunDetect/JaunDetect/Backend_Data/IDataRepository.cs
+++ JaunDetect/JaunDetect/Backend_Data/IDataRepository.cs
@@ -8,5 +8,21 @@ namespace JaunDetect.Backend_Data
 {
     interface IDataRepository
     {
+        // Methods needed to provide data for charts on the Resources page
+        #region Resources charts backend methods
+        string[] GetClinics();
+
+        int[] GetUsages();
+
+        string[] GetMonths();
+
+        int[] GetPatients();
+
+        double GetStripPrice();
+
+        bool UpdateStripPrice(double price);
+
+        double[] GetStripCosts();
+        #endregion
     }
 }
