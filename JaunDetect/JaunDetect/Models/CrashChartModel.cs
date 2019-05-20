d2da4ae3867a53cc1bd9199b9a009f7f088d016b
Laura Larson <larsonlaura@seattleu.edu>
2019-05-20 14:21:50 -0700

    Added attributes and partial initializer for CrashChart model

--- JaunDetect/JaunDetect/Models/CrashChartModel.cs
+++ JaunDetect/JaunDetect/Models/CrashChartModel.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using JaunDetect.Backend_Data;
 
 namespace JaunDetect.Models
 {
     public class CrashChartModel
     {
+        // Days going back from today
+        public string[] Days { get; set; }
+
+        // Months going back from this month
+        public string[] Months { get; set; }
+
+        // Number of crashes by time
+        public int[] CrashesByTime { get; set; }
+
+        // Number of crashes by type
+        public int[] CrashesByType { get; set; }
+
+        // Types of crashes
+        public string[] CrashTypes
+        { get; set; }
+
+        // Constructor
+        public CrashChartModel()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            RandomDataGenerator random = new RandomDataGenerator();
+            UnitGenerator unit = new UnitGenerator();
+
+            // init crashtypes
+            // init crashes by type
+            // init crashes by time
+            // init months
+            // init days
+        }
     }
 }
