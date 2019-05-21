c7ad0434fd55a4cdfa4916024e676fddc586a0f2
petriucmihai <mihaitp10@gmail.com>
2019-05-21 13:52:07 -0700

    Copied homepage from home controller to guest controller and from  home index to guest index

--- JaunDetect/JaunDetect/Controllers/GuestController.cs
+++ JaunDetect/JaunDetect/Controllers/GuestController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JaunDetect.Models;
+using JaunDetect.Backend_Data;
+using System.Web.Helpers;
 
 namespace JaunDetect.Controllers
 {
@@ -11,7 +14,70 @@ namespace JaunDetect.Controllers
         // GET: Guest
         public ActionResult Index()
         {
-            return View();
+            var homeModel = new HomeChartModel();
+
+            homeModel = DataBackend.Instance.GetHomeData();
+
+            return View(homeModel);
+        }
+
+        public ActionResult GetBilirubinLevelChart()
+        {
+            var homeModel = new HomeChartModel();
+            homeModel = DataBackend.Instance.GetHomeData();
+            int num = homeModel.TimeOption;
+
+            var key = new Chart(width: 900, height: 400)
+                .AddSeries(
+                    chartType: "column",
+                    name: homeModel.Clinics[0],
+                    xValue: homeModel.Timeframe[num],
+                    yValues: homeModel.BilirubinData[0])
+                .AddSeries(
+                    chartType: "column",
+                    name: homeModel.Clinics[1],
+                    xValue: homeModel.Timeframe[num],
+                    yValues: homeModel.BilirubinData[1])
+                .AddSeries(
+                    chartType: "column",
+                    name: homeModel.Clinics[2],
+                    xValue: homeModel.Timeframe[num],
+                    yValues: homeModel.BilirubinData[2])
+                .AddSeries(
+                    chartType: "column",
+                    name: homeModel.Clinics[3],
+                    xValue: homeModel.Timeframe[num],
+                    yValues: homeModel.BilirubinData[3])
+                .AddSeries(
+                    chartType: "column",
+                    name: homeModel.Clinics[4],
+                    xValue: homeModel.Timeframe[num],
+                    yValues: homeModel.BilirubinData[4])
+                .SetXAxis("Months")
+                .SetYAxis("Bilirubin Level (%)")
+                .AddLegend()
+                .Write();
+
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult UpdateHomeChartTimeOption(HomeChartModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataBackend.Instance.UpdateTimeOptionString(model.TimeOptionString);
+
+                var homeModel = new HomeChartModel();
+
+                homeModel = DataBackend.Instance.GetHomeData();
+
+                return View("Index", homeModel);
+            }
+
+            return View("Resources", model);
         }
     }
+
+
 }
