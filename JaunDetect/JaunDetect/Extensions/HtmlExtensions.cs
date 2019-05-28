cdf2da58cf628e1d10dde0347f40ae8af4df8dc9
petriucmihai <mihaitp10@gmail.com>
2019-05-28 10:38:54 -0700

    Added a method to HtmlExtensions to change the active tab based on which page the user is on

--- JaunDetect/JaunDetect/Extensions/HtmlExtensions.cs
+++ JaunDetect/JaunDetect/Extensions/HtmlExtensions.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace JaunDetect.Extensions
 {
-    public class HtmlExtensions
+    public static class HtmlExtensions
     {
+        public static string IsActive(this HtmlHelper htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+
+            return (controller == routeController && action == routeAction) ? "active" : "";
+        }
     }
 }
