fce3e82447c696a51bbc5720fd6cfac2c5c26cde
Onur Bakiner <onur.bakiner@gmail.com>
2019-05-31 11:40:26 -0700

    added more dates to make the visuals interesting

--- JaunDetect/JaunDetect/Backend_Data/QueryUserInputBackendRepository.cs
+++ JaunDetect/JaunDetect/Backend_Data/QueryUserInputBackendRepository.cs
@@ -7,21 +7,23 @@ namespace JaunDetect.Backend_Data
 {
     public class QueryUserInputBackendRepository : IQueryUserInputRepository
     {
-        const int ROWS = 100; // number of rows for the mockup database
+        const int ROWS = 200; // number of rows for the mockup database
         const double FAILURE_LIKELIHOOD = 0.05; // percentage of failed photos in the database
         TextInfo ti = new CultureInfo("en-US", false).TextInfo; // TextInfo instance is necessary for data to string conversion and manipulation
         
         // data for the mockup database
         string[] clinics = {"Lagos First Clinic", "Lagos Second Clinic", "Onitsha Clinic", "Kano Clinic", "Ibadan Clinic",
             "Uyo Clinic", "Nsukka Clinic", "Abuja First Clinic", "Abuja Second Clinic", "Aba Clinic"};
-        string[] provinces = {"Lagos", "Lagos", "Onitsha", "Kano", "Ibadan", "Uyo", "Nsukka", "Abuja", "Abuja", "Aba" };
-        string[] dates = {"1/1/2019", "1/15/2019", "2/2/2019", "2/15/2019", "3/3/2019", "3/15/201", "4/4/2019", "4/21/2019",
-                           "5/5/2019", "5/30/2019", "6/6/2019", "6/15/2019","7/7/2019", "7/28/2019", "8/8/2019", "8/21/2019",
-                           "9/9/2019", "10/10/2019", "11/1/2019", "12/31/2019" };
-        string[] devices = { "Samsung Galaxy S8", "Samsung Galaxy Note", "Sony Xperia", "Sony Xperia", "Tecno Spark", "Motorola One",
-                             "Huawei Y9", "Huawei P10", "Huawei P10", "OnePlus 5" };
-        string[] biliConcentrations = { "5%", "10%", "15%", "20%", "25%"};
-        string[] osList = { "9.0 Pie", "8.0 Oreo", "8.1 Oreo", "7.0 Nougat", "7.1 Nougat", "6.0 Marshmallow", "5.0 Lollipop", "4.4 KitKat",
+        string[] provinces = {"Lagos", "Lagos", "Onitsha", "Kano", "Ibadan", "Uyo", "Nsukka", "Abuja", "Abuja", "Aba"};
+        string[] dates = {"1/1/2018", "1/15/2018", "2/2/2018", "2/15/2018", "3/3/2018", "3/15/2018", "4/4/2018", "4/21/2018",
+                           "5/5/2018", "5/30/2018", "6/6/2018", "6/15/2018","7/7/2018", "7/28/2018", "8/8/2018", "8/21/2018",
+                           "9/9/2018", "10/10/2018", "11/1/2018", "12/31/2018","1/1/2019", "1/15/2019", "2/2/2019", "2/15/2019", "3/3/2019",
+                           "3/15/2019", "4/4/2019", "4/21/2019","5/5/2019", "5/30/2019", "6/6/2019", "6/15/2019","7/7/2019", "7/28/2019",
+                           "8/8/2019", "8/21/2019","9/9/2019", "10/10/2019", "11/1/2019", "12/31/2019"};
+        string[] devices = {"Samsung Galaxy S8", "Samsung Galaxy Note", "Sony Xperia", "Sony Xperia", "Tecno Spark", "Motorola One",
+                             "Huawei Y9", "Huawei P10", "Huawei P10", "OnePlus 5"};
+        string[] biliConcentrations = {"5%", "10%", "15%", "20%", "25%"};
+        string[] osList = {"9.0 Pie", "8.0 Oreo", "8.1 Oreo", "7.0 Nougat", "7.1 Nougat", "6.0 Marshmallow", "5.0 Lollipop", "4.4 KitKat",
                             "4.1 Jelly Bean", "4.0 Ice Cream Sandwich" };
         string[] photoList = {"~/Content/Images/badphoto1.jpg", "~/Content/Images/badphoto2.jpg", "~/Content/Images/badphoto3.jpg"};
 
